7ae6ee5234e9807c9bb280cbc012cab6734fc4f3
meckhonoshinkv <kvmekhonoshin@edu.hse.ru>
2024-04-24 17:11:53 +0500

    feat: реализовал на странице загрузки изображений настройку параметров для дальнейшей аугментации

--- client/src/views/ImageUploadView.vue
+++ client/src/views/ImageUploadView.vue
@@ -1,50 +1,122 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12">
-        <p>
-          Для начала аугментации загрузите изображения. Вы также можете загрузить их в виде
-          zip-архива.
-        </p>
-      </v-col>
-      <v-col cols="12">
-        <p>
-          Загружено изображений: {{ images.length }}
-        </p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-file-input
-          label="Выберите изображения или zip архив"
-          multiple
-          accept="image/*,.zip"
-          @change="handleFileInputChange"
-        ></v-file-input>
-      </v-col>
-    </v-row>
-    <v-row v-if="images.length > 0">
-      <v-col v-for="(image, index) in displayedImages" :key="index" cols="12" sm="6" md="4">
-        <v-img :src="image.url" contain></v-img>
-      </v-col>
-    </v-row>
-    <v-row v-if="images.length > 3">
-      <v-col cols="12">
-        <v-btn color="primary" @click="toggleImages">
-          {{ showAll ? 'Скрыть' : 'Показать все' }}
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row v-if="images.length > 0">
-      <v-col cols="12">
-        <p>
-          Отправьте загруженные изображения для аугментации
-        </p>
-      </v-col>
-      <v-btn color="primary" @click="uploadImages">Отправить изображения</v-btn>
-    </v-row>
+    <v-card>
+      <v-card-title>Аугментация изображений</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12">
+            <p>
+              Для начала аугментации загрузите изображения. Вы также можете загрузить их в виде
+              zip-архива.
+            </p>
+          </v-col>
+          <v-col cols="12">
+            <p>
+              Загружено изображений: {{ images.length }}
+            </p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-file-input color="primary" label="Выберите изображения или zip архив" multiple accept="image/*,.zip"
+              @change="handleFileInputChange"></v-file-input>
+          </v-col>
+        </v-row>
+        <v-row v-if="images.length > 0">
+          <v-col v-for="(image, index) in displayedImages" :key="index" cols="12" sm="6" md="4">
+            <v-img :src="image.url" contain></v-img>
+          </v-col>
+        </v-row>
+        <v-row v-if="images.length > 3">
+          <v-col cols="12">
+            <v-btn color="primary" @click="toggleImages">
+              <v-icon left>mdi-image</v-icon>
+              {{ showAll ? 'Скрыть' : 'Показать все' }}
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row v-if="images.length > 0">
+          <v-col cols="12">
+            <p>
+              Отправьте загруженные изображения для аугментации
+            </p>
+          </v-col>
+          <v-btn color="primary" @click="uploadImages">
+            <v-icon left>mdi-upload</v-icon>
+            Отправить изображения
+          </v-btn>
+          <v-row v-if="uploadSuccess">
+            <v-col cols="12">
+              <v-alert type="success" dense text>
+                Изображения успешно загружены
+              </v-alert>
+            </v-col>
+          </v-row>
+        </v-row>
+        <v-row v-if="images.length > 0 && uploadSuccess">
+          <v-col cols="12">
+            <p>
+              Настройте параметры аугментации
+            </p>
+          </v-col>
+          <v-col cols="12">
+            <v-row>
+              <v-col cols="8">
+                <v-slider label="Процент аугментации" v-model.number="augmentationPercentage" min="5" max="50"
+                  step="1"></v-slider>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field v-model.number="augmentationPercentage" readonly></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="8">
+                <v-slider label="Угол поворота" v-model.number="rotationAngle" min="0" max="45" step="1"></v-slider>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field v-model.number="rotationAngle" readonly></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="8">
+                <v-slider label="Шум" v-model.number="noise" min="0" max="50" step="1"></v-slider>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field v-model.number="noise" readonly></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="8">
+                <v-slider label="Контраст" v-model.number="contrast" min="0.5" max="1.5" step="0.1"></v-slider>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field v-model.number="contrast" readonly></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="8">
+                <v-slider label="Яркость" v-model.number="brightness" min="-255" max="255" step="1"></v-slider>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field v-model.number="brightness" readonly></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+
+          <v-btn color="primary" @click="startAugmentation">
+            <v-icon left>mdi-play</v-icon>
+            Начать аугментацию
+          </v-btn>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
+
 
 <script>
 import JSZip from 'jszip';
@@ -54,7 +126,13 @@ export default {
     return {
       files: [],
       images: [],
-      showAll: false
+      showAll: false,
+      uploadSuccess: false,
+      augmentationPercentage: 5,
+      rotationAngle: 0,
+      noise: 0,
+      contrast: 1,
+      brightness: 0
     }
   },
   computed: {
@@ -110,6 +188,7 @@ export default {
         const response = await this.$axios.post('/upload_images/', formData)
         // Обработка успешной загрузки
         console.log('Изображения успешно загружены:', response.data)
+        this.uploadSuccess = true
       } catch (error) {
         console.error('Ошибка загрузки изображений:', error)
       }
