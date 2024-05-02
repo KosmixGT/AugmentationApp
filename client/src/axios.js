8bff185a5e9706244c14602743d41b5baae6be19
Denis Gogolev <dvgogolev@edu.hse.ru>
2024-05-02 13:07:45 +0000

    Update client/src/axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 //const baseURL = 'http://backend:7505' // Замените на ваш URL сервера
 //const baseURL = 'http://gardunov.duckdns.org:7505'
-const baseURL = 'http://localhost:7505'
+//const baseURL = 'http://localhost:7505'
+const baseURL = 'http://backend:5000'
 
 const instance = axios.create({
   baseURL,
